94950da2d5b17dae5e0f95afd7e90c529c2700c1
Omar Gerardo Pérez Morales <omarg.perezm@gmail.com>
2025-01-17 11:59:16 -0600

    Agregar soporte de CORS

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const jsonServer = require('json-server');
+const cors = require('cors');
+
 const server = jsonServer.create();
 const router = jsonServer.router('db.json'); // Archivo db.json
 const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 8080; // Usar el puerto asignado por Render o 8080 por defecto
 
+// Middleware para habilitar CORS
+server.use(cors());
+
+// Otros middlewares de json-server
 server.use(middlewares);
 server.use(router);
 
+// Iniciar el servidor
 server.listen(PORT, () => {
   console.log(`JSON Server está corriendo en el puerto ${PORT}`);
 });
